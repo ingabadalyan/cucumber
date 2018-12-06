ddd6845636e5d59a43a6b34bd0ce3ca9734ef2c5
Inga Badalyan <inga.badalyan@nbwm.nl>
2018-12-06 10:00:09 +0100

    Pick up the day from the calendars

---
+++ src/test/selenium/CalendarPickUpDay.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class CalendarPickUpDay {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+
+        driver.get("http://path2usa.com/travel-companions");
+        // Pick up July 15 from the calendar
+        driver.findElement(By.xpath(".//*[@id='travel_date']")).click();
+
+        List<WebElement> listOfDays = driver.findElements(By.cssSelector(".day"));
+        for (WebElement listOfDay : listOfDays) {
+
+            String text = listOfDay.getText();
+            if (text.equalsIgnoreCase("15")) {
+                listOfDay.click();
+                break;
+            }
+        }
+    }
+}
